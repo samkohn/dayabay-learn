95b5a783d640329a95634ee23bd5cc79b3833a16
Sam Kohn <kohn@berkeley.edu>
2017-06-07 16:34:09 -0700

    Fix bug in order of shape parameters.
    Relies on ~/.keras/keras.json containing "data_format": "channels_first"

--- networks/BasicConvAE.py
+++ networks/BasicConvAE.py
@@ -16,33 +16,33 @@ def get_model(bottleneck_width):
     num_features = 8 * 24 * 2
     init = keras.initializers.RandomNormal(mean=0,stddev=1.0/num_features)
 
-    model.add(ZeroPadding2D((2, 2), input_shape=(8, 24, 2)))
-    # input shape = (12, 28, 2)
+    model.add(ZeroPadding2D((2, 2), input_shape=(2, 8, 24)))
+    # input shape = (2, 12, 28)
     model.add(Conv2D(128, (5, 5), activation='relu', kernel_initializer=init))
-    # input shape = (8, 24, 128)
+    # input shape = (128, 8, 24)
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    # input shape = (4, 12, 128)
+    # input shape = (128, 4, 12)
     model.add(ZeroPadding2D((1, 0)))
-    # input shape = (6, 12, 128)
+    # input shape = (128, 6, 12)
     model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer=init))
-    # input shape = (4, 10, 128)
+    # input shape = (128, 4, 10)
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    # input shape = (2, 5, 128)
+    # input shape = (128, 2, 5)
     model.add(Conv2D(bottleneck_width, (2, 5), activation='relu',
         kernel_initializer=init))
 
-    # middle of network. input shape = (1, 1, bottleneck_width)
+    # middle of network. input shape = (bottleneck_width, 1, 1)
     model.add(Conv2DTranspose(128, (2, 4), strides=(2, 2), activation='relu',
-        kernel_initializer=init))
-    # input shape = (2, 4, 128)
+        kernel_initializer=init, data_format=keras.backend.image_data_format()))
+    # input shape = (128, 2, 4)
     model.add(Conv2DTranspose(128, (2, 5), strides=(2, 2), activation='relu',
-        kernel_initializer=init))
-    # input shape = (4, 11, 128)
+        kernel_initializer=init, data_format=keras.backend.image_data_format()))
+    # input shape = (128, 4, 11)
     model.add(Conv2DTranspose(2, (2, 4), strides=(2, 2), activation='relu',
-        kernel_initializer=init))
-    #output shape = (8, 24, 2)
+        kernel_initializer=init, data_format=keras.backend.image_data_format()))
+    #output shape = (2, 8, 24)
     return model
 
 def compile_model(model):
