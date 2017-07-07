5fddcbd4dde4705b9650d6d6f1a848a8ad3008ee
Sam Kohn <kohn@berkeley.edu>
2017-07-06 17:42:33 -0700

    Add argparse functionality to BasicConv

--- train_acc_ibd_autoencoder.py
+++ train_acc_ibd_autoencoder.py
@@ -6,22 +6,40 @@ import networks.preprocessing as preprocessing
 from util.data_loaders import get_ibd_data
 import callbacks as cb
 import logs
+import argparse
 
-num_pairs = 9000
-logger = logs.get_tee_logger('test.log')
+parser = argparse.ArgumentParser()
+parser.add_argument('-n', '--numpairs', type=int, default=1000,
+        help='number of IBD/event pairs total to use')
+parser.add_argument('-w', '--bottleneck-width', type=int, default=16,
+        help='number of features in the bottleneck layer')
+parser.add_argument('--master-logfile', default='master.log',
+        help='logfile to keep track of runs')
+parser.add_argument('--run-logfile', default='test.log',
+        help='logfile to monitor this run')
+parser.add_argument('-o', '--output', default='.',
+        help='folder to save all files generated in this run')
+parser.add_argument('--save-interval', type=int, default=10,
+        help='number of epochs between saving intermediate output')
+
+args = parser.parse_args()
+logger = logs.get_tee_logger(args.run_logfile)
+logs.log_with_git_hash(' '.join(sys.argv), args.master_logfile)
+output_folder = args.output
+numpairs = args.numpairs
+save_interval = args.save_interval
+
 logger.info('Beginning training module')
 
-logfile = 'runs.log'
-logs.log_with_git_hash(' '.join(sys.argv), logfile)
-
-folder = '.'
 # Load ibd and accidental data
-train_ibd, _, _ = get_ibd_data(tot_num_pairs=num_pairs, just_charges=True,
+num_ibds = numpairs/2
+num_acc = numpairs/2
+train_ibd, _, _ = get_ibd_data(tot_num_pairs=num_ibds, just_charges=True,
         train_frac=1, valid_frac=0)
 train_acc, _, _ = \
 get_ibd_data(path='/project/projectdirs/dasrepo/ibd_pairs/accidentals.h5',
         h5dataset='accidentals_bg_data',
-        tot_num_pairs=num_pairs, just_charges=True, train_frac=1, valid_frac=0)
+        tot_num_pairs=num_acc, just_charges=True, train_frac=1, valid_frac=0)
 train_set = np.vstack((train_ibd, train_acc))
 
 # Preprocessing: set min, max to -1, 1
@@ -38,8 +56,9 @@ tensorboard = nn.keras.callbacks.TensorBoard(log_dir='batch/logs/tensorboard', w
 callbacks = [
         tensorboard,
         cb.WriteToLogger(logger),
-        cb.SaveIntermediateResults(train_set, models, folder, 5),
-        cb.SaveInputs(train_set, folder)
+        cb.SaveIntermediateResults(train_set, models, output_folder,
+            save_interval),
+        cb.SaveInputs(train_set, output_folder)
         ]
 results = autoencoder.fit(train_set, train_set, epochs=10, batch_size=128,
         callbacks=callbacks)
